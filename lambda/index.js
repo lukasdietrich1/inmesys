16c9dd2c2eb226c53f7ee9954701ec5daa67a4cd
lukasdietrich1 <lukasdietrich93@yahoo.de>
2020-05-25 15:02:13 +0200

    updates to index and interactionmodel

--- lambda/index.js
+++ lambda/index.js
@@ -8,7 +8,7 @@ const LaunchRequestHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Welcome, you can say Hello or Help. Which would you like to try?';
+        const speakOutput = 'this is the adventure castle, what do you want to do';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .reprompt(speakOutput)
